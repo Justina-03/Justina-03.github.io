f229c207fde0af815cdb4cb263a764791b1db1da
Junghyun Kim <kimjunghyun2003@gmail.com>
2024-10-11 13:38:22 +0000

    css 수정

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// 각 섹션의 HTML 내용을 변수에 저장
-const honorsContent = document.querySelector('.honors').innerHTML;
-const projectsContent = document.querySelector('.projects').innerHTML;
-const extracurricularContent = document.querySelector('.extracurricular').innerHTML;
-
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
@@ -22,27 +17,21 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 
-    // 클릭된 탭에 맞는 내용을 로드
-    if (tabName === 'Honors') {
-        document.getElementById('Honors').innerHTML = honorsContent;
-    } else if (tabName === 'Projects') {
-        document.getElementById('Projects').innerHTML = projectsContent;
-    } else if (tabName === 'Extracurricular') {
-        document.getElementById('Extracurricular').innerHTML = extracurricularContent;
-    }
-
-    // Home 탭일 때는 모든 섹션을 보이도록
+    // Home 탭일 때 모든 섹션을 보이도록
     if (tabName === 'Home') {
         document.querySelector('.honors').style.display = 'block';
         document.querySelector('.projects').style.display = 'block';
-        document.querySelector('.extracurricular').style.display = 'block';
+        document.querySelector('.extracurriculars').style.display = 'block';
     } else {
-        // Home 탭이 아니면, 다른 섹션들은 숨기기
+        // Home 탭이 아니면 다른 섹션들은 숨기기
         document.querySelector('.honors').style.display = 'none';
         document.querySelector('.projects').style.display = 'none';
-        document.querySelector('.extracurricular').style.display = 'none';
+        document.querySelector('.extracurriculars').style.display = 'none';
     }
 }
 
-// 기본적으로 Home 탭을 보여주기
+// 페이지가 처음 로드되면 기본적으로 Home 탭을 보여주기
 document.getElementById('Home').style.display = 'block';
+document.querySelector('.honors').style.display = 'block';
+document.querySelector('.projects').style.display = 'block';
+document.querySelector('.extracurriculars').style.display = 'block';
